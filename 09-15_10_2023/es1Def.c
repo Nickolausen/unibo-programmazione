b247cb67cddda26f6cf6b19b8282a5f3687f0f71
Ventru04 <146826585+Ventru04@users.noreply.github.com>
2023-10-14 17:19:20 +0200

    versione definitiva
    magi tanto lo ricontrolla per i commenti in inglese vedi se quel dormi funziona come ho scritto nei commenti anche a te

--- 09-15_10_2023/es1Def.c
+++ 09-15_10_2023/es1Def.c
@@ -1,3 +1,4 @@
+//varie librerie utilizzate
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -8,6 +9,7 @@
 
 // Per separare nel terminale
 #define LINE_SPLITTER puts("\n-------------")
+//per pulire la console
 #define CLEAR_CONSOLE system("clear") == 1 ? system("cls") : system("clear")
 
 /*
@@ -18,6 +20,7 @@
     > Riccardo Ventrucci <riccardo.ventrucci@studio.unibo.it>;
 */
 
+//creo le struct che mi servono per dopo
 struct Coordinate
 {
     int latitudine;
@@ -31,26 +34,18 @@ struct Continente
     struct Coordinate confineBassoDestro;
 };
 
+//creo i metodi per rendere il codice del main più pulito e meno ripetitivo
 bool ControlloGiusto(struct Continente cont, struct Coordinate coor)
 {
-    if (cont.confineBassoDestro.latitudine < coor.latitudine && cont.confineAltoSinistro.latitudine > coor.latitudine)
+    if (cont.confineBassoDestro.latitudine <= coor.latitudine && cont.confineAltoSinistro.latitudine >= coor.latitudine)
     {
-        if (cont.confineAltoSinistro.longitudine < coor.longitudine && cont.confineBassoDestro.longitudine > coor.longitudine)
+        if (cont.confineAltoSinistro.longitudine <= coor.longitudine && cont.confineBassoDestro.longitudine >= coor.longitudine)
         {
             return true;
         }
     }
     return false;
 }
-
-void Dormi(int tempo)
-{
-    for (int i = 0; i < tempo * pow(10.0, 7); i++)
-    {
-    }
-    CLEAR_CONSOLE;
-}
-
 bool Controllo(char risp[7], struct Continente cont, struct Coordinate daIndovinare)
 {
     if (strcmp(risp, cont.nome) == 0)
@@ -64,9 +59,22 @@ bool Controllo(char risp[7], struct Continente cont, struct Coordinate daIndovin
     return false;
 }
 
+//per non utilizzare la funzione sleep
+void Dormi(int tempo)
+{
+    for (int i = 0; i < tempo * pow(10.0, 7); i++){
+    }
+    CLEAR_CONSOLE;
+}
+
+
+
 int main()
 {
     // continenti modello ONU quindi 5
+    //dichiaro tutti i continenti con le loro coordinate, ho semplificato rendendoli tutti dei rettangoli
+    //ma anche cambiando le coordinate rimanendo tra 0-100 di latitudine e 0-250 di longitudine senza 
+    //sovrapporre i continenti funziona comunque
     struct Continente america;
     strcpy(america.nome, "america");
     america.confineAltoSinistro.latitudine = 100;
@@ -98,34 +106,36 @@ int main()
     oceania.confineBassoDestro.latitudine = 0;
     oceania.confineBassoDestro.longitudine = 250;
 
+    //dichiaro il random per estrarre le coordinate
     srand(time(0));
+    //dichiaro l'int in cui metterò il timer
     int start;
+    //dichiaro la variabile delle coordinate da indovinare
+    struct Coordinate daIndovinare;
+
+    //finchè non indovina il ciclo và avanti
     while (true)
     {
-        printf("\nIn 3 secondi ti verranno mostrate le coordinate, indovina in quale dei 5 continenti è");
-        Dormi(5);
-        printf("\n3");
-        Dormi(2);
-        printf("\n2");
-        Dormi(2);
-        printf("\n1");
-        printf("\nVIA!!!");
-        Dormi(1);
-
-        struct Coordinate daIndovinare;
+        //estraggo le coordinate
         daIndovinare.latitudine = rand() % 100 + 1;
         daIndovinare.longitudine = rand() % 100 * 2.5 + 1;
+
+        //le faccio vedere temporaneamente
         printf("\nLa latidine è: %d La longitudine è:%d", daIndovinare.latitudine, daIndovinare.longitudine);
-        Dormi(10);
+
+        //intanto starto il cronometro
         start = time(NULL);
-        printf("\nIndovina il continente (quando hai finito di scrivere il continente premi ctrl+Z/D): ");
+
+        //chiedo di indovinare
+        printf("\nIndovina il continente (quando hai finito di scrivere il continente premi invio): ");
+        //non mettendo qui questa funzione non mi cleara le coordinate e non aspetta un po'
+        //di tempo con il ciclo intutile 
+        Dormi(13);
+        //lo prendo in input
         char contRisp[7];
-        int numCar = 0;
-        while (getchar() != EOF)
-        {
-            numCar++;
-        }
         scanf("%s", contRisp);
+
+        //controllo se ha scritto giusto e dove sono le coordinate estratte
         if (Controllo(contRisp, america, daIndovinare))
             break;
         if (Controllo(contRisp, europa, daIndovinare))
@@ -136,10 +146,13 @@ int main()
             break;
         if (Controllo(contRisp, oceania, daIndovinare))
             break;
+        //se ha sbagliato riprova a indovinare
         printf("\nHai sbagliato \nRiprova con delle coordinate nuove");
-        Dormi(1);
+        Dormi(2);
     }
-    int tempo = start - time(NULL);
-    printf("Ci hai messo %d ", tempo);
+    //quando è uscito calcolo il tempo che ci ha messo per la risposta corrente
+    int tempo = time(NULL) - start;
+    //e lo mando in output
+    printf("\nCi hai messo %d secondi\n", tempo);
     return 0;
 }
