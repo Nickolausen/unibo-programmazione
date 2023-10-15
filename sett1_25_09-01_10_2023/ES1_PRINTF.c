71cc53743da516f05b0b81d476abb065943c9bad
Nicholas Magi <nicholas.magi24@gmail.com>
2023-10-15 15:44:02 +0200

    updated ES1_PRINTF.c

--- sett1_25_09-01_10_2023/ES1_PRINTF.c
+++ sett1_25_09-01_10_2023/ES1_PRINTF.c
@@ -1,88 +1,75 @@
 #include <stdio.h>
-#include <limits.h> // Library which includes the maximum and minimum values for: (s/uns)char, (s/uns)short, (s/uns)int, (s/uns)long;
-#include <float.h> // Library which includes, other than float and double characteristics, constants for their minimum and maximum values;
-#define LINE_SPLITTER puts("\n-------------") // Just a puts with some dashes to separate each data type description
+#include <limits.h>
+#include <float.h>
+#define LINE_SPLITTER puts("----------")
 
 /*
     Group components:
     > Nicholas Magi <nicholas.magi@studio.unibo.it>;
     > Tommaso Maestri <tommaso.maestri@studio.unibo.it>;
-    > Riccardo Ventrucci <riccardo.ventrucci@studio.unibo.it>;
     > Leonardo Perretta <leonardo.perretta2@studio.unibo.it>;
+    > Riccardo Ventrucci <riccardo.ventrucci@studio.unibo.it>;
 */
 
-int main() 
-{
-    /*
-        Primary data types:
-        > float
-        > int
-        > double
-        > char
-    */
-    signed char signedCharVar = 'A';
-    unsigned char unsignedCharVar = 'F';
+void printNrChars(int nrChars) {
+    printf("Numero di caratteri stampati: %d\n", nrChars);
+    LINE_SPLITTER;
+    return;
+}
+
+int main() {
+    // Dichiarazione e inizializzazione delle variabili
+    char charVar = 'A';
+    unsigned char ucharVar = 255;
+    short shortVar = 32767;
+    unsigned short ushortVar = 65535;
+    int intVar = INT_MAX;
+    unsigned int uintVar = UINT_MAX;
+    long longVar = LONG_MAX;
+    unsigned long ulongVar = ULONG_MAX;
+    float floatVar = FLT_MAX;
+    double doubleVar = DBL_MAX;
+    char stringVar[] = { 'H', 'e', 'l', 'l', 'o', ' ', '!'};
     
-    signed short signedShortVar = -30;
-    unsigned short unsignedShortVar = 25;
+    // Utilizzo di tutti gli argomenti della printf() studiati
+    printNrChars(printf("Modificatori:\n"));
+    printNrChars(printf("%%d: %d\n", intVar));
+    printNrChars(printf("%%6d: %6d\n", intVar));
+    printNrChars(printf("%%.2f: %.2f\n", doubleVar));
+    printNrChars(printf("%%c: %c\n", charVar));
+    printNrChars(printf("%%s: %s\n", stringVar));
+    printNrChars(printf("%%10s: %10s\n", stringVar));
+    printNrChars(printf("%%-10s: %-10s\n", stringVar));
+    printNrChars(printf("%%5.3f: %5.3f\n", doubleVar));
+    printNrChars(printf("%%+d: %+d\n", intVar));
+    printNrChars(printf("%%#x: %#x\n", 255));
+    printNrChars(printf("%%o: %o\n", 255));
+    printNrChars(printf("%%lld: %lld\n", 123456789012345LL));
+
+    // Stampa dei valori e dell'occupazione di memoria con il numero di caratteri stampati
+    printNrChars(printf("char: %c, Occupazione: %lu byte\n", charVar, sizeof(charVar)));
+    printNrChars(printf("unsigned char: %u, Occupazione: %lu byte\n", ucharVar, sizeof(ucharVar)));
+    printNrChars(printf("short: %hi, Occupazione: %lu byte\n", shortVar, sizeof(shortVar)));
+    printNrChars(printf("unsigned short: %hu, Occupazione: %lu byte\n", ushortVar, sizeof(ushortVar)));
+    printNrChars(printf("int: %d, Occupazione: %lu byte\n", intVar, sizeof(intVar)));
+    printNrChars(printf("unsigned int: %u, Occupazione: %lu byte\n", uintVar, sizeof(uintVar)));
+    printNrChars(printf("long: %ld, Occupazione: %lu byte\n", longVar, sizeof(longVar)));
+    printNrChars(printf("unsigned long: %lu, Occupazione: %lu byte\n", ulongVar, sizeof(ulongVar)));
+    printNrChars(printf("float: %f, Occupazione: %lu byte\n", floatVar, sizeof(floatVar)));
+    printNrChars(printf("double: %lf, Occupazione: %lu byte\n", doubleVar, sizeof(doubleVar)));
     
-    signed int signedIntVar = -10;
-    unsigned int unsignedIntVar = 10;
-    
-    signed long singedLongVar = -2500;
-    unsigned long unsignedLongVar = 3200;
-    
-    float floatVar = 1.5;
-    double doubleVar = -1.25;
-    long double longDoubleVar = 100.25;
-
-    /* 
-        In order to print the number of characters displayed for each type's description,
-        we used the fact that the function "printf" returns the number of characters it puts out.
-    */ 
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %u \nMIN VALUE: %d \nMAX VALUE: %u \nBYTE: %lu\n", "Unsigned Char", "unsignedCharVar", unsignedCharVar, 0, UCHAR_MAX, sizeof(unsigned char)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %c \nMIN VALUE: %d \nMAX VALUE: %d \nBYTE: %lu\n", "Signed Char", "signedCharVar", signedCharVar, SCHAR_MIN, SCHAR_MAX, sizeof(signed char)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %d \nMIN VALUE: %d \nMAX VALUE: %d \nBYTE: %lu\n", "Signed Short", "signedShortVar", signedShortVar, SHRT_MIN, SHRT_MAX, sizeof(short)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %u \nMIN VALUE: %d \nMAX VALUE: %u \nBYTE: %lu\n", "Unsigned Short", "unsignedShortVar", unsignedShortVar, 0, USHRT_MAX, sizeof(unsigned short)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %d \nMIN VALUE: %d \nMAX VALUE: %d \nBYTE: %lu\n", "Signed Int", "signedIntVar", signedIntVar, INT_MIN, INT_MAX, sizeof(int)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %u \nMIN VALUE: %d \nMAX VALUE: %u \nBYTE: %lu\n", "Unsigned Int", "unsignedIntVar", unsignedIntVar, 0, UINT_MAX, sizeof(unsigned int)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %ld \nMIN VALUE: %ld \nMAX VALUE: %ld \nBYTE: %lu\n", "Signed Long", "singedLongVar", singedLongVar, LONG_MIN, LONG_MAX, sizeof(long)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %lu \nMIN VALUE: %d \nMAX VALUE: %lu \nBYTE: %lu\n", "Unsigned Long", "unsignedLongVar", unsignedLongVar, 0, ULONG_MAX, sizeof(unsigned long)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %f \nMIN VALUE: %e \nMAX VALUE: %e \nBYTE: %lu\n", "Float", "floatVar", floatVar, FLT_MIN, FLT_MAX, sizeof(float)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %f \nMIN VALUE: %e \nMAX VALUE: %e \nBYTE: %lu\n", "Double", "doubleVar", doubleVar, DBL_MIN, DBL_MAX, sizeof(double)));
-    LINE_SPLITTER;
-
-    printf("> PRINTED CHARACTERS: %d", 
-        printf("TYPE: %s \nVARIABLE NAME: %s \nVALUE: %.10e \nMIN VALUE: %.10e \nMAX VALUE: %.10e \nBYTE: %lu\n", "Long Double", "longDoubleVar", longDoubleVar, LDBL_MIN, LDBL_MAX, sizeof(long double)));
-    LINE_SPLITTER;
+    // Stampa dei valori massimi e minimi ammissibili
+    printNrChars(printf("\nValori massimi e minimi per variabili di diversi tipi:\n"));
+    printNrChars(printf("char: Min: %d, Max: %d\n", CHAR_MIN, CHAR_MAX));
+    printNrChars(printf("unsigned char: Min: %d, Max: %d\n", 0, UCHAR_MAX));
+    printNrChars(printf("short: Min: %d, Max: %d\n", SHRT_MIN, SHRT_MAX));
+    printNrChars(printf("unsigned short: Min: %d, Max: %u\n", 0, USHRT_MAX));
+    printNrChars(printf("int: Min: %d, Max: %d\n", INT_MIN, INT_MAX));
+    printNrChars(printf("unsigned int: Min: %u, Max: %u\n", 0, UINT_MAX));
+    printNrChars(printf("long: Min: %ld, Max: %ld\n", LONG_MIN, LONG_MAX));
+    printNrChars(printf("unsigned long: Min: %d, Max: %lu\n", 0, ULONG_MAX));
+    printNrChars(printf("float: Min: %E, Max: %E\n", FLT_MIN, FLT_MAX));
+    printNrChars(printf("double: Min: %E, Max: %E\n", DBL_MIN, DBL_MAX));
 
     return 0;
 }
