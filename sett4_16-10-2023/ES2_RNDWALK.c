29cd6d286b8291be34b70b3daf9831d2db0899a1
Nickolausen <nicholas.magi24@gmail.com>
2023-10-23 23:30:58 +0200

    fixed rnd walk

---
+++ sett4_16-10-2023/ES2_RNDWALK.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <limits.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h> 
+#include <string.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define SLEEP(ms) Sleep(ms * 1000)
+#define CLEAR_CONSOLE system("cls")
+#else
+#include <unistd.h>
+#define SLEEP(s) sleep(s)
+#define CLEAR_CONSOLE system("clear")
+#endif
+#define LINE_SPLITTER puts("\n\n----------")
+
+/*
+    Group components:
+    > Nicholas Magi <nicholas.magi@studio.unibo.it>;
+    > Tommaso Maestri <tommaso.maestri@studio.unibo.it>;
+    > Leonardo Perretta <leonardo.perretta2@studio.unibo.it>;
+    > Riccardo Ventrucci <riccardo.ventrucci@studio.unibo.it>;
+*/
+
+void matrix_print(char matrix[10][10])
+{
+    printf("\n");
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%c", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void matrix_fill(char matrix[10][10])
+{ 
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            matrix[i][j] = '.';
+        }
+    }
+}
+
+bool compute_legal_moves(int position[2], char matrix[10][10])
+{
+    bool legalMoves = false;
+    int x = position[1];
+    int y = position[0];
+    int countLegalMoves = 4;
+
+    //controllo mosse possibili sull'asse orizzontale
+    if(x-1 < 0 )
+        countLegalMoves--;
+    else
+        if(matrix[y][x-1] != '.')
+            countLegalMoves--;
+    if(x+1 > 9)
+        countLegalMoves--;
+    else
+        if(matrix[y][x+1] != '.')
+            countLegalMoves--;
+        
+    //controllo mosse possibili sull'asse verticale
+    if(y-1 < 0 )
+        countLegalMoves--;
+    else
+        if(matrix[y-1][x] != '.')
+            countLegalMoves--;
+    if(y+1 > 9)
+        countLegalMoves--;
+    else
+        if(matrix[y+1][x] != '.')
+            countLegalMoves--;
+
+    if(countLegalMoves > 0)
+        legalMoves = true;
+    return legalMoves;
+}
+
+int main()
+{
+    srand(time(NULL));
+    char matrix[10][10];
+    bool game = true;
+    char last_letter = 'A';
+    char alphabet[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    int alphabetPosition = 1;
+    int position[2] = {0,0};
+    matrix_fill(matrix);
+    matrix[0][0] = last_letter;
+
+    char message[100] = "Camminata completata! Tutte le lettere dell'alfabeto sono state correttamente posizionate";
+
+    do
+    {
+        bool legalMoves = compute_legal_moves(position, matrix);
+        if(legalMoves)
+        {
+            int x, y;
+            x = position[1];
+            y = position[0];
+            bool legalMove = false;
+            do
+            {
+                legalMove = false;
+                char dir;
+                // u = su = 0, d = giù = 1, l = sinistra = 2, r = destra = 3
+                int r = rand() % 4;
+                if(r == 0)
+                    dir = 'u';
+                else if (r == 1)
+                    dir = 'd';
+                else if (r == 2)
+                    dir = 'l';
+                else
+                    dir = 'r';   
+
+                if(dir == 'u')
+                {
+                    if(y - 1 >= 0 && matrix[y-1][x] == '.')
+                    {
+                        legalMove = true;
+                        y = --position[0];
+                    }
+                }
+                else if(dir == 'd')
+                {
+                    if(y + 1 <= 9 && matrix[y+1][x] == '.')
+                    {
+                        legalMove = true;
+                        y = ++position[0];
+                        
+                    }                      
+                }
+                else if(dir == 'l')
+                {
+                    if(x - 1 >= 0 && matrix[y][x-1] == '.')
+                    {
+                        legalMove = true;
+                        x = --position[1];
+                    }                        
+                }
+                else
+                {
+                    if(x + 1 <= 9 && matrix[y][x+1] == '.')
+                    {
+                        legalMove = true;
+                        x = ++position[1];
+                    }                     
+                }
+            } while (!legalMove);
+            matrix[y][x] = alphabet[alphabetPosition];
+            last_letter = alphabet[alphabetPosition++];
+        }
+        else
+        {
+            strcpy(message, "Nessuna mossa disponibile! Gioco terminato.");
+            game = false;
+        }
+    } while (game && last_letter != 'Z');
+
+    printf("%s", message);
+    matrix_print(matrix);
+
+    return 0;
+}
--- sett5_23-10-2023/ES1_DECRIPTAZIONE.c
+++ sett5_23-10-2023/ES1_DECRIPTAZIONE.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #ifdef _WIN32
@@ -11,8 +12,8 @@
 #define CLEAR_CONSOLE system("clear")
 #endif
 #define LINE_SPLITTER puts("\n\n----------")
-#define LENGTH(a)(int)(sizeof(a)/sizeof(a[0]))
-#define RND_CHAR (rand()%(126-32))+32
+#define LENGTH(a) (int)(sizeof(a) / sizeof(a[0]))
+#define RND_CHAR (rand() % (126 - 32)) + 32
 #define STRINGA_INIZIALE "e' difficile programmare in C quando ti hanno insegnato con C#..."
 #define CARATTERI_PER_VOLTA 3
 #define INTERVALLO_DI_TEMPO 0.1
@@ -25,50 +26,54 @@
     > Riccardo Ventrucci <riccardo.ventrucci@studio.unibo.it>;
 */
 
-int main() {
+int main()
+{
     char stringaIniziale[] = STRINGA_INIZIALE;
     char stringaCriptata[LENGTH(stringaIniziale)];
-    int caratteriSostituiti[LENGTH(stringaIniziale)-1];
+    int caratteriSostituiti[LENGTH(stringaIniziale) - 1];
     srand(time(NULL));
 
-    //imposto carattere di terminazione alla stringa criptata
-    stringaCriptata[LENGTH(stringaIniziale)-1] = '\0';
-    //riempio la stringa criptata di char random e imposto i valori di caratteriSostituiti a 0
-    for(int i = 0; i<LENGTH(stringaIniziale)-1; i++) {
+    // imposto carattere di terminazione alla stringa criptata
+    stringaCriptata[LENGTH(stringaIniziale) - 1] = '\0';
+    // riempio la stringa criptata di char random e imposto i valori di caratteriSostituiti a 0
+    for (int i = 0; i < LENGTH(stringaIniziale) - 1; i++)
+    {
         stringaCriptata[i] = RND_CHAR;
         caratteriSostituiti[i] = 0;
     }
 
-    int daSostituire = LENGTH(stringaIniziale)-1;
+    int daSostituire = LENGTH(stringaIniziale) - 1;
     int i, indiceDaSostituire;
-    //ripeto finché ci sono valori da sostituire
-    while(daSostituire>0) {
+    // ripeto finché ci sono valori da sostituire
+    while (daSostituire > 0)
+    {
 
         CLEAR_CONSOLE;
         printf("%s", stringaCriptata);
         SLEEP(INTERVALLO_DI_TEMPO);
         i = 0;
 
-        //sostiuisco n caratteri per ogni stampa
-        while(i<CARATTERI_PER_VOLTA)
+        // sostiuisco n caratteri per ogni stampa
+        while (i < CARATTERI_PER_VOLTA)
         {
             indiceDaSostituire;
-            do {
+            do
+            {
                 /* genero randomicamente un indice da sostituire all'interno della stringa criptata
                 e controllo di non aver già sostituito in quella posizione */
-                indiceDaSostituire = (rand()%(LENGTH(stringaIniziale)-1));
-            } while(caratteriSostituiti[indiceDaSostituire] == 1);
+                indiceDaSostituire = (rand() % (LENGTH(stringaIniziale) - 1));
+            } while (caratteriSostituiti[indiceDaSostituire] == 1);
 
-            //sostituisco nella stringa criptata l'indice generato con il rispettivo carattere giusto
+            // sostituisco nella stringa criptata l'indice generato con il rispettivo carattere giusto
             stringaCriptata[indiceDaSostituire] = stringaIniziale[indiceDaSostituire];
-            //segno in caratteriSostituiti l'indice che ho sostituito
+            // segno in caratteriSostituiti l'indice che ho sostituito
             caratteriSostituiti[indiceDaSostituire] = 1;
             daSostituire--;
             i++;
             /*se non ci sono più caratteri da sostituire ma non ho sostituto abbastanza caratteri
             per questo giro di stampa, esco comunque dal ciclo*/
-            if(daSostituire==0)
-                i=CARATTERI_PER_VOLTA;
+            if (daSostituire == 0)
+                i = CARATTERI_PER_VOLTA;
         }
     }
 
