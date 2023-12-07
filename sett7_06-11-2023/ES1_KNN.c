e467f95f60de33ce97e97edb02f42dad359e43a5
Ventru04 <146826585+Ventru04@users.noreply.github.com>
2023-12-07 10:20:03 +0100

    Da ricaricare
    dio porco

--- sett7_06-11-2023/ES1_KNN.c
+++ sett7_06-11-2023/ES1_KNN.c
@@ -63,77 +63,9 @@ int IsOneOfTheNearest(Pair near[], int puntiTemp, int k)
     return -1;
 }
 
-void PrintObject(int index, int similarity_score, Loan* pObject) 
-{
-    printf("Oggetto [%d] - punti di similarità: %d/13\n", index, similarity_score);
-    puts("{");
-    printf("\t%s: %s,\n", "\"credit_policy\"", pObject->credit_policy);
-    printf("\t%s: %s,\n", "\"purpose\"", pObject->purpose);
-    printf("\t%s: %s,\n", "\"int_rate\"", pObject->int_rate);
-    printf("\t%s: %s,\n", "\"installments\"", pObject->installments);
-    printf("\t%s: %s,\n", "\"long_annual_inc\"", pObject->log_annual_inc);
-    printf("\t%s: %s,\n", "\"dti\"", pObject->dti);
-    printf("\t%s: %s,\n", "\"fico\"", pObject->fico);
-    printf("\t%s: %s,\n", "\"days_with_cr_line\"", pObject->days_with_cr_line);
-    printf("\t%s: %s,\n", "\"revol_bal\"", pObject->revol_bal);
-    printf("\t%s: %s,\n", "\"revol_util\"", pObject->revol_util);
-    printf("\t%s: %s,\n", "\"inq_last_6mths\"", pObject->inq_last_6mths);
-    printf("\t%s: %s,\n", "\"delinq_2yrs\"", pObject->delinq_2yrs);
-    printf("\t%s: %s,\n", "\"pub_rec\"", pObject->pub_rec);
-    printf("\t%s: %s,\n", "\"not_fully_paid\"", pObject->not_fully_paid);
-    puts("}");
-    puts("");
-}
-
-int main()
-{
-    int k;
-    Loan standard;
-// region inizializzazione standard
-#pragma region
-    standard.credit_policy = "0";
-    standard.purpose = "debt_consolidation";
-    standard.int_rate = "0.15";
-    standard.installments = "986.3";
-    standard.log_annual_inc = "13.036823";
-    standard.dti = "7.89";
-    standard.fico = "679";
-    standard.days_with_cr_line = "3674.9";
-    standard.revol_bal = "16749";
-    standard.revol_util = "30.11";
-    standard.inq_last_6mths = "2";
-    standard.delinq_2yrs = "1";
-    standard.pub_rec = "0";
-    standard.not_fully_paid = "0";
-#pragma endregion
-
-    // Prendo in input quanti valori vicini vuole l'utente
-    printf("\nQuanti vicini (k) vuoi visualizzare del valore di default? ");
-    scanf("%d", &k);
-
-    // Inizializzo tutti i valori di nearest a -1 cosìcche sicuramente ci saranno k 
-    // elementi nell'array
-    Pair nearest[k];
-    for (int i = 0; i < k; i++)
-    {
-        nearest[i].puntiSimilarita = -1;
-    }
+void Nearest(FILE *file, Pair nearest[],Loan standard,int k){
     
-    //Apro il file con un percorso non relativo, quindi da cambiare con il file messo nello zip
-    FILE *file;
-    char filename[] = "/Users/nickolausen/Library/CloudStorage/OneDrive-AlmaMaterStudiorumUniversitàdiBologna/UNI/1o Anno/Programmazione/Repo_ESERCIZI/elaborati-programmazione/sett7_06-11-2023/DS-Credit/loan_data.csv"; // Inserisci il nome del TUO file CSV
-
-    // Apro il file
-    file = fopen(filename, "r");
-    
-    // Se il file è stato aperto male interrompo l'esecuzione e mando un "messaggio d'errore"
-    if (file == NULL)
-    {
-        printf("Impossibile aprire il file %s\n", filename);
-        return 1;
-    }
-
-    // Dichiaro le variabili che mi serviranno durante l'esecuzione principale
+     // Dichiaro le variabili che mi serviranno durante l'esecuzione principale
     char line[MAX_ROW][MAX_LEN];
     long long col = 0;
     double puntiSimilarita;
@@ -257,6 +189,80 @@ int main()
         col++;
     }
 
+}
+
+void PrintObject(int index, int similarity_score, Loan* pObject) 
+{
+    printf("Oggetto [%d] - punti di similarità: %d/13\n", index, similarity_score);
+    puts("{");
+    printf("\t%s: %s,\n", "\"credit_policy\"", pObject->credit_policy);
+    printf("\t%s: %s,\n", "\"purpose\"", pObject->purpose);
+    printf("\t%s: %s,\n", "\"int_rate\"", pObject->int_rate);
+    printf("\t%s: %s,\n", "\"installments\"", pObject->installments);
+    printf("\t%s: %s,\n", "\"long_annual_inc\"", pObject->log_annual_inc);
+    printf("\t%s: %s,\n", "\"dti\"", pObject->dti);
+    printf("\t%s: %s,\n", "\"fico\"", pObject->fico);
+    printf("\t%s: %s,\n", "\"days_with_cr_line\"", pObject->days_with_cr_line);
+    printf("\t%s: %s,\n", "\"revol_bal\"", pObject->revol_bal);
+    printf("\t%s: %s,\n", "\"revol_util\"", pObject->revol_util);
+    printf("\t%s: %s,\n", "\"inq_last_6mths\"", pObject->inq_last_6mths);
+    printf("\t%s: %s,\n", "\"delinq_2yrs\"", pObject->delinq_2yrs);
+    printf("\t%s: %s,\n", "\"pub_rec\"", pObject->pub_rec);
+    printf("\t%s: %s,\n", "\"not_fully_paid\"", pObject->not_fully_paid);
+    puts("}");
+    puts("");
+}
+
+int main()
+{
+    int k;
+    Loan standard;
+// region inizializzazione standard
+#pragma region
+    standard.credit_policy = "0";
+    standard.purpose = "debt_consolidation";
+    standard.int_rate = "0.15";
+    standard.installments = "986.3";
+    standard.log_annual_inc = "13.036823";
+    standard.dti = "7.89";
+    standard.fico = "679";
+    standard.days_with_cr_line = "3674.9";
+    standard.revol_bal = "16749";
+    standard.revol_util = "30.11";
+    standard.inq_last_6mths = "2";
+    standard.delinq_2yrs = "1";
+    standard.pub_rec = "0";
+    standard.not_fully_paid = "0";
+#pragma endregion
+
+    // Prendo in input quanti valori vicini vuole l'utente
+    printf("\nQuanti vicini (k) vuoi visualizzare del valore di default? ");
+    scanf("%d", &k);
+
+    // Inizializzo tutti i valori di nearest a -1 cosìcche sicuramente ci saranno k 
+    // elementi nell'array
+    Pair nearest[k];
+    for (int i = 0; i < k; i++)
+    {
+        nearest[i].puntiSimilarita = -1;
+    }
+    
+    //Apro il file con un percorso non relativo, quindi da cambiare con il file messo nello zip
+    FILE *file;
+    char filename[] = "/Users/riccardo/Documents/GitHub/elaborati-programmazione/sett7_06-11-2023/DS-Credit/loan_data.csv"; // Inserisci il nome del TUO file CSV
+
+    // Apro il file
+    file = fopen(filename, "r");
+    
+    // Se il file è stato aperto male interrompo l'esecuzione e mando un "messaggio d'errore"
+    if (file == NULL)
+    {
+        printf("Impossibile aprire il file %s\n", filename);
+        return 1;
+    }
+
+    Nearest(file, nearest, standard, k);
+   
     printf("\n>> Oggetti più vicini trovati: \n");
     for (int i = 0; i < k; i++)
     {
